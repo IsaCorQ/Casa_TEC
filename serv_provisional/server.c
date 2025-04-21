2ed5da0e2ecc26f87c5e560fb1f55b09049a6f29
Dylan Gerardo Garbanzo Fallas <dgarbanzof@estudiantec.cr>
2025-04-21 15:41:37 -0600

    Pequeño fix

--- serv_provisional/server.c
+++ serv_provisional/server.c
@@ -205,7 +205,7 @@ int main() {
     }
     
     int opt = 1;
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
         perror("setsockopt");
         exit(EXIT_FAILURE);
     }
